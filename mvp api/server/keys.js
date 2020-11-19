07a8a4440bb8a024d2409c8e4bc47e6d11d7e5a3
kevin1920 <56529861+kevin1920@users.noreply.github.com>
2020-11-18 22:10:26 -0500

    Se agregaron variables de entorno y la clase pg

---
+++ mvp api/server/keys.js
@@ -0,0 +1,6 @@
+//Variables de entorno
+process.env.USERDB = "postgres"
+process.env.HOSTDB = "localhost"
+process.env.DB = "mvpSoftware3"
+process.env.PASSWORD = "alexyaleja"
+process.env.PORT_DB = 5432
